f4b9414a587b000381a69a9d0c1b88fbd24fc32a
Awigo <awizen.adam@gmail.com>
2021-05-24 18:13:19 +0200

    initial commit with 'hello spring'

---
+++ src/test/java/com/example/springbootwithamigo/SpringbootwithamigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwithamigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwithamigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
