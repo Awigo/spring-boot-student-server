5e9808e03344445fc9ee0210e9920f3c02b44a57
Awigo <awizen.adam@gmail.com>
2021-05-24 18:32:06 +0200

    add StudentController.java

--- src/main/java/com/example/springbootwithamigo/SpringbootwithamigoApplication.java
+++ src/main/java/com/example/springbootwithamigo/SpringbootwithamigoApplication.java
@@ -2,21 +2,13 @@ package com.example.springbootwithamigo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class SpringbootwithamigoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootwithamigoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootwithamigoApplication.class, args);
+    }
 
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello spring!";
-	}
 
 }
