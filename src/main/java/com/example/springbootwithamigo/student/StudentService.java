d531d2e166f0e8a2ba1961ba73e0c7ec5c2c4efd
Awigo <awizen.adam@gmail.com>
2021-05-25 09:47:13 +0200

    add StudentRepository to StudentService

--- src/main/java/com/example/springbootwithamigo/student/StudentService.java
+++ src/main/java/com/example/springbootwithamigo/student/StudentService.java
@@ -1,25 +1,21 @@
 package com.example.springbootwithamigo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service
 public class StudentService {
 
-    @GetMapping()
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Mariam",
-                        "mariam.jamal@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY, 5),
-                        21
-                )
-        );
+        return studentRepository.findAll();
     }
 }
